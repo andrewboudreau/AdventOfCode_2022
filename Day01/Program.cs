2fe6409f3c1189d4c0f9af9f3dfb1463c911540a
Andrew Boudreau <andrewboudreau@gmail.com>
2022-11-20 19:03:03 -0600

    just getting the basics ready for 2022

---
+++ Day01/Program.cs
@@ -0,0 +1,27 @@
+﻿using static Day00.ReadInputs;
+
+Console.WriteLine(
+    ReadInts()
+        .Aggregate(
+            seed: (prev: int.MaxValue, increases: 0),
+            func: (acc, next) =>
+                (next, (next > acc.prev)
+                    ? acc.increases + 1
+                    : acc.increases)));
+
+
+var prev = new int[3] { int.MaxValue, int.MaxValue, int.MaxValue, };
+
+Console.WriteLine(ReadInts().Aggregate(
+    seed: (prev, increases: 0),
+    func: (acc, next) =>
+    {
+        acc.increases +=
+            next + acc.prev[2] + acc.prev[1] > acc.prev[0] + acc.prev[1] + acc.prev[2]
+                ? 1 : 0;
+
+        acc.prev[0] = acc.prev[1];
+        acc.prev[1] = acc.prev[2];
+        acc.prev[2] = next;
+        return acc;
+    }).increases - 1);
