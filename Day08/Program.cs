7e1318ad22e7f34b693b2ba9b7a2a53596f541a3
Andrew Boudreau <andrewboudreau@gmail.com>
2022-12-07 22:33:06 -0600

    adding examples for usable code parts

--- Day08/Program.cs
+++ Day08/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Day00;
+using Day00.Solvers;
+
+using static Day00.ReadInputs;
+
+var day08 = new Day08();
+Read()
+    .SolveWith(solveWith: day08)
+    .ToConsole("\r\nDay 08 Solution");
+
+public class Day08 : Solution
+{
+    public override ISolve Load(string? data)
+    {
+        return this;
+    }
+
+    public override ISolve Solve()
+    {
+        return this;
+    }
+}
